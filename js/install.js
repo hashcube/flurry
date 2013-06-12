dcf36cd520eae174746027ff62ae1b33b2be4aa6
Mario Balibrera <mario@gameclosure.com>
2013-06-12 12:47:52 -0700

    extra check for NATIVE.plugins.sendEvent

--- js/install.js
+++ js/install.js
@@ -1,7 +1,8 @@
 GLOBAL.flurry = {
 	logEvent: function(evtName, evtParams) {
 		logger.log("{flurry} logEvent: ", evtName, evtParams);
-		NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent("FlurryPlugin",
-			"logEvent", JSON.stringify({ eventName: evtName, params: evtParams }));
+		NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
+			NATIVE.plugins.sendEvent("FlurryPlugin", "logEvent",
+				JSON.stringify({ eventName: evtName, params: evtParams }));
 	}
 };
