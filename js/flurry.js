e848667736ac2925578432fc427e2e441d50a1b6
Martin Hunt <martin@gameclosure.com>
2013-09-04 12:20:25 -0700

    add support for setGlobalProperty

--- js/flurry.js
+++ js/flurry.js
@@ -1,10 +1,29 @@
+var hasNativeEvents = NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent;
+
 var Flurry = Class(function () {
+	this.init = function () {
+		this._globalProperties = {};
+	}
+
 	this.track = function (name, data) {
-		logger.log("{flurry} track: ", name, JSON.stringify(data));
-		NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
-			NATIVE.plugins.sendEvent("FlurryPlugin", "track",
-				JSON.stringify({ eventName: name, params: data }));
+		// copy in global properties
+		merge(data, this._globalProperties);
+
+		if (DEBUG) {
+			logger.log("track: ", name, JSON.stringify(data));
+		}
+
+		if (hasNativeEvents) {
+			NATIVE.plugins.sendEvent("FlurryPlugin", "track", JSON.stringify({
+					eventName: name,
+					params: data
+				}));
+		}
 	};
+
+	this.setGlobalProperty = function (key, value) {
+		this._globalProperties[key] = value;
+	}
 });
 
 exports = new Flurry();
