3d9937135b165a341812f07a4759df8283aa551c
Collin Green <collin@gameclosure.com>
2014-05-15 16:37:53 -0700

    Moves code from flurry.js to index.js

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var hasNativeEvents = NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent;
+
+var Flurry = Class(function () {
+	this.init = function () {
+		this._globalProperties = {};
+	}
+
+	this.trackEvent =
+	this.track = function (name, data) {
+        if (!data) {
+            data = {};
+        }
+
+		// copy in global properties
+		merge(data, this._globalProperties);
+
+		if (DEBUG) {
+			logger.log("track: ", name, JSON.stringify(data));
+		}
+
+		if (hasNativeEvents) {
+			NATIVE.plugins.sendEvent("FlurryPlugin", "track", JSON.stringify({
+					eventName: name,
+					params: data
+				}));
+		}
+	};
+
+	this.setUserId = function (userId) {
+		if (DEBUG) {
+			logger.log("setUserId: ", userId);
+		}
+
+		if (hasNativeEvents) {
+			NATIVE.plugins.sendEvent("FlurryPlugin", "setUser", JSON.stringify({
+					user: userId
+				}));
+		}
+	};
+
+	this.setGlobalProperty = function (key, value) {
+		this._globalProperties[key] = value;
+	}
+});
+
+exports = new Flurry();
