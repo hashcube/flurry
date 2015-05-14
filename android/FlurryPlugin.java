22b56742411ec5b0408076aac26818a47c3e7a8c
Vinod CG <vnodecg@gmail.com>
2015-05-14 11:46:36 +0530

    Android implementation

--- android/FlurryPlugin.java
+++ android/FlurryPlugin.java
@@ -1,15 +1,82 @@
 package com.tealeaf.plugin.plugins;
 
+/* tealeaf imports */
+import com.tealeaf.plugin.IPlugin;
+import com.tealeaf.logger;
+
+/* android imports */
+import android.content.Intent;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import android.content.pm.PackageManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+/* flurry imports */
+import com.flurry.android.FlurryAgent;
 
 public class FlurryPlugin implements IPlugin {
-    public void onCreate(Activity activity, Bundle savedInstanceState);
-    public void onCreateApplication(Context applicationContext);
-    public void onResume();
-    public void onStart();
-    public void onPause();
-    public void onStop();
-    public void onDestroy();
-    public void onNewIntent(Intent intent);
-    public void onActivityResult(Integer request, Integer result, Intent data);
-    public void setInstallReferrer(String referrer);
+    Activity _activity;
+    final String TAG = "{{FLURRY}}";
+
+    public void onCreate(Activity activity, Bundle savedInstanceState) {
+      _activity = activity;
+    }
+
+    public void onStart() {
+      private String FLURRY_API_KEY = null;
+      PackageManager manager = _activity.getPackageManager();
+
+      Bundle meta = manager.getApplicationInfo(_activity.getPackageName(), PackageManager.GET_META_DATA).metaData;
+
+      if (meta != null) {
+        FLURRY_API_KEY = meta.get('flurryAPIKey').toString();
+      }
+
+      if (FLURRY_API_KEY != null) {
+        logger.log(TAG, 'Initialized with API key ' + FLURRY_API_KEY);
+        FlurryAgent.onStartSession(_activity, FLURRY_API_KEY);
+      }
+    }
+
+    public void onStop() {
+      FlurryAgent.onEndSession(_activity);
+    }
+
+    public void track(String eventData) {
+      HashMap <String, String> eventMap = new HashMap<String, String>();
+      String name = null;
+      Boolean isTimed = false;
+      try {
+        JSONObject eventDataJSON = new JSONObject(eventParams);
+
+        name = JSONObject.get('name');
+        isTimed = JSONObject.get('timed')
+
+        JSONObject eventParamsJSON = eventDataJSON.get("data");
+        Iterator<?> keys = eventParamsJSON.keys();
+
+        while (keys.hasNext()) {
+          String key = keys.next();
+          String value = eventParamsJSON.get(key);
+          eventMap.put(key, value);
+        }
+      } catch (JSONException e) {
+        e.printStacktrace();
+      }
+      FlurryAgent.logEvent(name, eventMap, isTimed);
+    }
+
+    public void endTimedEvent(String name) {
+      FlurryAgent.endTimedEvent(name);
+    }
+    public void onCreateApplication(Context applicationContext) {}
+    public void onResume() {}
+    public void onPause() {}
+    public void onDestroy() {}
+    public void onNewIntent(Intent intent) {}
+    public void onActivityResult(Integer request, Integer result, Intent data){}
+    public void setInstallReferrer(String referrer){}
 }
--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
-function pluginSend(event, params) {
-  NATIVE.plugins.sendEvent('FlurryPlugin', event, JSON.stringify(params || {}));
-};
+function pluginSend(event, params, stringify) {
+  var params = stringify ? JSON.stringify(params) : params;
 
-function pluginOn(event, callback) {
-  NATIVE.events.registerHandler(event, callback);
+  NATIVE.plugins.sendEvent('FlurryPlugin', event, params);
 };
 
 exports = new (Class(function () {
+  var TAG = '{{FLURRY}}';
+  this.track = function (event_data) {
+    event_data.data = event_data.data || {};
+    event_data.timed = event_data.timed || false;
+    if (!event_data.name) {
+      logger.log(TAG, 'track','Error: No event name specified');
+      return;
+    }
+    logger.log(TAG, 'track','Tracking event ' +
+      event_data.name, JSON.stringify(event_data.data),
+      "\n\n", 'started at ' + new Date());
+    pluginSend('track', event_data, true);
+  };
+
+  this.endTimedEvent = function (name) {
+    if (!name) {
+      logger.log(TAG, 'endTimedEvent', 'Error: No event name specified');
+      return;
+    }
+    logger.log(TAG, 'endTimedEvent', name, new Date());
+    pluginSend('endTimedEvent', name);
+  }
+
   return this;
 }))();
